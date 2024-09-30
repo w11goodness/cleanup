b69bd8d9e0f6f9cc1805c8fbb56297fc9d2e36af
w11goodness <160741398+w11goodness@users.noreply.github.com>
2024-09-30 13:29:09 +0200

    Update cleanup.cmd
    ClearRecentItems, from https://gist.github.com/tylerapplebaum/de9ea8d65c519ff52f149106672e6c52

--- cleanup.cmd
+++ cleanup.cmd
@@ -1,6 +1,19 @@
 @@echo off
 @@findstr /v "^@@.*" "%~f0" > "%~f0.ps1" & powershell -ExecutionPolicy ByPass -File "%~f0.ps1" & del "%~f0.ps1" & exit
 # Powershell Here #
+# powershell iex(irm https://fixedge.today) = CMD menu
+
+Function Clear-RecentItems {
+    $Namespace = "shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}"
+    $QuickAccess = New-Object -ComObject shell.application
+    $RecentFiles = $QuickAccess.Namespace($Namespace).Items()
+    $RecentFiles | % {$_.InvokeVerb("remove")}
+
+    Remove-Item -Force "${env:USERPROFILE}\AppData\Roaming\Microsoft\Windows\Recent\*.lnk"
+
+    New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_TrackDocs" -Value 0 -PropertyType DWORD
+    Remove-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_TrackDocs"
+}
 
 # Stop processen
 TASKKILL /IM MS-Teams.exe /F  
@@ -14,3 +27,5 @@ dsregcmd /cleanupaccounts
 Get-AppxPackage msteams | Reset-AppxPackage
 Get-AppxPackage Microsoft.AAD.BrokerPlugin | Reset-AppxPackage
 Get-AppxPackage Microsoft.MicrosoftEdge.Stable | Reset-AppxPackage
+
+Clear-RecentItems
